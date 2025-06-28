a26c4022c45dc8397f20e1d9ab6132d71840b5fb
MFW2000 <marijnwelmerink@gmail.com>
2025-06-28 14:10:35 +0200

    Add tests for the ReadString method

---
+++ MFW.PasswordGeneratorTests/Helpers/PromptHelperTests.cs
@@ -0,0 +1,104 @@
+﻿using MFW.PasswordGenerator.Helpers;
+
+namespace MFW.PasswordGeneratorTests.Helpers;
+
+[TestClass]
+public class PromptHelperTests
+{
+    [TestMethod]
+    public void ReadString_WithAllowEmpty_ReturnsEmptyString()
+    {
+        // Arrange
+        var expectedString = string.Empty;
+
+        var consoleInput = new StringReader("\n");
+
+        Console.SetIn(consoleInput);
+
+        // Act
+        var result = PromptHelper.ReadString(allowEmpty: true);
+
+        // Assert
+        Assert.AreEqual(expectedString, result);
+    }
+
+    [TestMethod]
+    public void ReadString_WithoutAllowEmpty_ThrowsArgumentException()
+    {
+        // Arrange
+        var consoleInput = new StringReader("\n");
+
+        Console.SetIn(consoleInput);
+
+        // Act & Assert
+        Assert.ThrowsExactly<ArgumentException>(() => PromptHelper.ReadString(allowEmpty: false));
+    }
+
+    [TestMethod]
+    public void ReadString_WithTrim_ReturnsTrimmedString()
+    {
+        // Arrange
+        const string inputString = " untrimmed string ";
+        const string expectedString = "untrimmed string";
+
+        var consoleInput = new StringReader($"{inputString}\n");
+
+        Console.SetIn(consoleInput);
+
+        // Act
+        var result = PromptHelper.ReadString(trim: true);
+
+        // Assert
+        Assert.AreEqual(expectedString, result);
+    }
+
+    [TestMethod]
+    public void ReadString_WithoutTrim_ReturnsUntrimmedString()
+    {
+        // Arrange
+        const string inputExpectedString = " untrimmed string ";
+
+        var consoleInput = new StringReader($"{inputExpectedString}\n");
+
+        Console.SetIn(consoleInput);
+
+        // Act
+        var result = PromptHelper.ReadString(trim: false);
+
+        // Assert
+        Assert.AreEqual(inputExpectedString, result);
+    }
+
+    [TestMethod]
+    public void ReadString_WithinMaxLength_ReturnsString()
+    {
+        // Arrange
+        const string inputExpectedString = "12345";
+        const int maxLength = 5;
+
+        var consoleInput = new StringReader($"{inputExpectedString}\n");
+
+        Console.SetIn(consoleInput);
+
+        // Act
+        var result = PromptHelper.ReadString(maxLength: maxLength);
+
+        // Assert
+        Assert.AreEqual(inputExpectedString, result);
+    }
+
+    [TestMethod]
+    public void ReadString_OverMaxLength_ThrowsArgumentOutOfRangeException()
+    {
+        // Arrange
+        const string inputString = "123456";
+        const int maxLength = 5;
+
+        var consoleInput = new StringReader($"{inputString}\n");
+
+        Console.SetIn(consoleInput);
+
+        // Act & Assert
+        Assert.ThrowsExactly<ArgumentOutOfRangeException>(() => PromptHelper.ReadString(maxLength: maxLength));
+    }
+}
