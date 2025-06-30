a96fe39d2459cf3c6b7f661297072b388a405463
MFW2000 <marijnwelmerink@gmail.com>
2025-06-30 23:51:45 +0200

    Added console logging service

---
+++ MFW.PasswordGenerator/Infrastructure/ConsoleLogger.cs
@@ -0,0 +1,54 @@
+﻿using MFW.PasswordGenerator.Infrastructure.Interfaces;
+
+namespace MFW.PasswordGenerator.Infrastructure;
+
+public class ConsoleLogger : IConsoleLogger
+{
+    private static readonly object Lock = new();
+
+    /// <inheritdoc/>
+    public void LogDebug(string message, string logFile = Constants.DefaultLogFile)
+    {
+        Log("DBG", message, logFile);
+    }
+
+    /// <inheritdoc/>
+    public void LogInformation(string message, string logFile = Constants.DefaultLogFile)
+    {
+        Log("INF", message, logFile);
+    }
+
+    /// <inheritdoc/>
+    public void LogWarning(string message, string logFile = Constants.DefaultLogFile)
+    {
+        Log("WRN", message, logFile);
+    }
+
+    /// <inheritdoc/>
+    public void LogError(string message, string logFile = Constants.DefaultLogFile)
+    {
+        Log("ERR", message, logFile);
+    }
+
+    /// <inheritdoc/>
+    public void LogCritical(string message, string logFile = Constants.DefaultLogFile)
+    {
+        Log("FTL", message, logFile);
+    }
+
+    private static void Log(string level, string message, string logFile)
+    {
+        if (string.IsNullOrWhiteSpace(message))
+        {
+            logFile = Constants.DefaultLogFile;
+        }
+
+        var timestamp = DateTimeOffset.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        var logEntry = $"[{timestamp} {level}] {message}";
+
+        lock (Lock)
+        {
+            File.AppendAllText(logFile, logEntry + Environment.NewLine);
+        }
+    }
+}
---
+++ MFW.PasswordGenerator/Infrastructure/Interfaces/IConsoleLogger.cs
@@ -0,0 +1,54 @@
+﻿namespace MFW.PasswordGenerator.Infrastructure.Interfaces;
+
+public interface IConsoleLogger
+{
+    /// <summary>
+    /// Logs a debug message to the log file.
+    /// </summary>
+    /// <param name="message">
+    /// Format the string of the log message in the message template format.
+    /// Example: <c>"User {User} logged in from {Address}"</c>.
+    /// </param>
+    /// <param name="logFile">Optional log file path. Defaults to <see cref="Constants.DefaultLogFile"/>.</param>
+    void LogDebug(string message, string logFile = Constants.DefaultLogFile);
+
+    /// <summary>
+    /// Logs an information message to the log file.
+    /// </summary>
+    /// <param name="message">
+    /// Format the string of the log message in the message template format.
+    /// Example: <c>"User {User} logged in from {Address}"</c>.
+    /// </param>
+    /// <param name="logFile">Optional log file path. Defaults to <see cref="Constants.DefaultLogFile"/>.</param>
+    void LogInformation(string message, string logFile = Constants.DefaultLogFile);
+
+    /// <summary>
+    /// Logs a warning message to the log file.
+    /// </summary>
+    /// <param name="message">
+    /// Format the string of the log message in the message template format.
+    /// Example: <c>"User {User} logged in from {Address}"</c>.
+    /// </param>
+    /// <param name="logFile">Optional log file path. Defaults to <see cref="Constants.DefaultLogFile"/>.</param>
+    void LogWarning(string message, string logFile = Constants.DefaultLogFile);
+
+    /// <summary>
+    /// Logs an error message to the log file.
+    /// </summary>
+    /// <param name="message">
+    /// Format the string of the log message in the message template format.
+    /// Example: <c>"User {User} logged in from {Address}"</c>.
+    /// </param>
+    /// <param name="logFile">Optional log file path. Defaults to <see cref="Constants.DefaultLogFile"/>.</param>
+    void LogError(string message, string logFile = Constants.DefaultLogFile);
+
+    /// <summary>
+    /// Logs a fatal message to the log file.
+    /// </summary>
+    /// <param name="message">
+    /// Format the string of the log message in the message template format.
+    /// Example: <c>"User {User} logged in from {Address}"</c>.
+    /// </param>
+    /// <param name="logFile">Optional log file path. Defaults to <see cref="Constants.DefaultLogFile"/>.</param>
+    void LogCritical(string message, string logFile = Constants.DefaultLogFile);
+}
